ec46ea21392fcafcc16ef303a5aafc842e039f59
AntooGuevara <jose_3141240215@utd.edu.mx>
2025-06-27 18:31:45 -0600

    Delete 2_modulos_paquetes directory

--- 2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludos("Angel Jaimes"))
-
-nombre,tel=modulos.datos2()
-print(f"\n\tAgenda telelfónica\n\t\tNombre: {nombre}\n\t\tTelefóno: {tel}")
-
-modulos.esperetecla()
